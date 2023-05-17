db921194802cc19c9fe86c853f3a0f3e778042ef
Mattias Forsman <forsman1991@gmail.com>
2023-05-17 17:25:25 +0200

    Update PHP 8 compatability

--- src/Map.php
+++ src/Map.php
@@ -9,19 +9,19 @@ abstract class Map implements ArrayAccess {
 		$this->data = $data;
 	}
 
-	public function offsetExists($offset) {
+	public function offsetExists($offset): bool {
 		return isset($this->data[$offset]);
 	}
 
-	public function offsetGet($offset) {
+	public function offsetGet($offset): mixed {
 		return $this->data[$offset] ?? null;
 	}
 
-	public function offsetSet($offset, $value) {
+	public function offsetSet($offset, $value): void {
 		$this->data[$offset] = $value;
 	}
 
-	public function offsetUnset($offset) {
+	public function offsetUnset($offset): void {
 		unset($this->data[$offset]);
 	}
 
